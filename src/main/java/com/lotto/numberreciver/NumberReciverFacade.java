dbbe1a57385789b95eeafd946b4771e6ccf93fdc
kopkalucas <kopka.lucas@gmail.com>
2023-06-01 16:29:53 +0200

    prepered api's

---
+++ src/main/java/com/lotto/numberreciver/NumberReciverFacade.java
@@ -0,0 +1,11 @@
+package com.lotto.numberreciver;
+
+import java.util.List;
+
+public class NumberReciverFacade {
+
+    public String inputNumbers(List<Integer> numbersFromUser) {
+
+        return "Success";
+    }
+}
