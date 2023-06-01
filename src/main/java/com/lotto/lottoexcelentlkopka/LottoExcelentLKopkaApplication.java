86c3b4d60bcefab88661f0bcb7ab94659e43bd47
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-01 16:05:22 +0200

    git init

---
+++ src/main/java/com/lotto/lottoexcelentlkopka/LottoExcelentLKopkaApplication.java
@@ -0,0 +1,13 @@
+package com.lotto.lottoexcelentlkopka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoExcelentLKopkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoExcelentLKopkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lotto/lottoexcelentlkopka/LottoExcelentLKopkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto.lottoexcelentlkopka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoExcelentLKopkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
